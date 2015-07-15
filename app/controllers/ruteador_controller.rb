af18d7ffa18c9da8454c7576b58c72094020facf
Juan Carlos <krlos013@gmail.com>
2015-07-15 00:42:17 -0500

    10

--- app/controllers/ruteador_controller.rb
+++ app/controllers/ruteador_controller.rb
@@ -3,8 +3,8 @@ class RuteadorController < ApplicationController
   	if user_signed_in?
   		redirect_to welcome_path
   	end
-  	if admin_signed_in?
-  		redirect_to brokers_path
-  	end
+  	# if admin_signed_in?
+  	# 	redirect_to brokers_path
+  	# end
   end
 end
