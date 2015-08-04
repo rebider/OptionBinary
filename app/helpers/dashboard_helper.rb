dda8ee7f68673eb5771cbbe5015ad91cf29d77a6
Miguel Bahena <miguel_bahena@hotmail.com>
2015-08-03 21:11:15 -0500

    Ok

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -4,7 +4,7 @@ module DashboardHelper
 		tie_by_day = Trade.where(:User_id => current_user.id).where(result: 'TIE').trades_grouped_by_date(start)
 		lost_by_day = Trade.where(:User_id => current_user.id).where(result: 'LOST').trades_grouped_by_date(start)
 
-		(start.to_date..Date.today).map do |date|
+		(start.to_date..Time.zone.today).map do |date|
 			{
 				created_at: date,
 				total_win: win_by_day[date].try(:first).try(:total_trades) || 0,
@@ -15,15 +15,15 @@ module DashboardHelper
 	end
 
 	def today_trades_chart_data
-		win_by_day = Trade.where(:User_id => current_user.id).where(result: 'WIN').trades_grouped_by_date(Date.today.in_time_zone)
-		tie_by_day = Trade.where(:User_id => current_user.id).where(result: 'TIE').trades_grouped_by_date(Date.today.in_time_zone)
-		lost_by_day = Trade.where(:User_id => current_user.id).where(result: 'LOST').trades_grouped_by_date(Date.today.in_time_zone)
+		win_by_day = Trade.where(:User_id => current_user.id).where(result: 'WIN').trades_grouped_by_date(Time.zone.today)
+		tie_by_day = Trade.where(:User_id => current_user.id).where(result: 'TIE').trades_grouped_by_date(Time.zone.today)
+		lost_by_day = Trade.where(:User_id => current_user.id).where(result: 'LOST').trades_grouped_by_date(Time.zone.today)
 
 
 
-		[ { :label => 'WIN', :value => win_by_day[Date.today].try(:first).try(:total_trades) || 0 },
-		  { :label => 'TIE', :value => tie_by_day[Date.today].try(:first).try(:total_trades) || 0 },
-		  { :label => 'LOST', :value => lost_by_day[Date.today].try(:first).try(:total_trades) || 0 }
+		[ { :label => 'WIN', :value => win_by_day[Time.zone.today].try(:first).try(:total_trades) || 0 },
+		  { :label => 'TIE', :value => tie_by_day[Time.zone.today].try(:first).try(:total_trades) || 0 },
+		  { :label => 'LOST', :value => lost_by_day[Time.zone.today].try(:first).try(:total_trades) || 0 }
 		 ]
 
 	end
