1eb921ccd6cb743470e28a7678cdc21e318c17d9
Juan Carlos Santiago <krlos013@gmail.com>
2015-08-04 16:19:48 -0500

    Cambio en Account_Balance

--- db/migrate/20150804203824_alter_account_balance.rb
+++
@@ -1,7 +0,0 @@
-class AlterAccountBalance < ActiveRecord::Migration
-  def change
-
-  	remove_column :account_balances, :BrokerAccount_id, :integer
-	add_reference :account_balances, :broker_account, index: true, foreign_key: true
-  end
-end
