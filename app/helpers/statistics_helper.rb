af29bb9bb843af94f36a86f03d9e24e163efbb02
Juan Carlos Santiago <krlos013@gmail.com>
2015-08-04 16:53:22 -0500

    Para que e mike me agrege el campo

--- app/helpers/statistics_helper.rb
+++ app/helpers/statistics_helper.rb
@@ -1,2 +1,16 @@
 module StatisticsHelper
+	def trades_chart_data(start = 3.weeks.ago)
+		win_by_day  = Trade.where(:User_id => current_user.id).where(result: 'WIN').trades_grouped_by_date(start)
+		tie_by_day  = Trade.where(:User_id => current_user.id).where(result: 'TIE').trades_grouped_by_date(start)
+		lost_by_day = Trade.where(:User_id => current_user.id).where(result: 'LOST').trades_grouped_by_date(start)
+
+		(start.to_date..Time.zone.today).map do |date|
+			{
+				created_at: date,
+				total_win:  win_by_day[date].try(:first).try(:total_trades)  || 0,
+				total_tie:  tie_by_day[date].try(:first).try(:total_trades)  || 0,
+				total_lost: lost_by_day[date].try(:first).try(:total_trades) || 0,
+			}
+		end
+	end
 end
