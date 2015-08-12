5b0a2424c18fec8458545b32362315a88f7b8229
Juan Carlos Santiago <krlos013@gmail.com>
2015-08-11 23:53:00 -0500

    11-08-2015 graficas parte 10

--- app/controllers/statistics_controller.rb
+++ app/controllers/statistics_controller.rb
@@ -1,48 +1,8 @@
 class StatisticsController < ApplicationController
   def index
-  	######### grafica 1
+
   	@start = 3.weeks.ago
   	@fechaf = Time.zone.today
-	# 	(@start.to_date..Time.zone.today).each do |s|
-	# 		@f_salida = s.to_date
-	# 		@win      = Trade.where(:User_id => current_user.id).where("created_at LIKE (?)", "#{@f_salida}%" ).where(result: "WIN").group("date(created_at)") 
-			
-	# 	Rails.logger.info "Fechas #{@f_salida} "
-	# 	Rails.logger.info "Win #{@win.count} "
-
-			
-	# end
-
-
-  	@categorias = Trade.where(:User_id => current_user.id).where(created_at: "2015-07-15"..Time.zone.now).group("date(created_at)") 
-  	@win        = Trade.where(:User_id => current_user.id).where(created_at: "2015-07-15"..Time.zone.now).where(result: "WIN").group("date(created_at)") 
-  	@tie        = Trade.where(:User_id => current_user.id).where(created_at: "2015-07-15"..Time.zone.now).where(result: "TIE").group("date(created_at)") 
-  	@lost       = Trade.where(:User_id => current_user.id).where(created_at: "2015-07-15"..Time.zone.now).where(result: "LOST").group("date(created_at)") 
-  
-  	
-  	
-    ######### fin grafica 1 #####
-    ######### grafica 2
-  	@categorias1  = Trade.where(:User_id => current_user.id).where(created_at: "2015-07-15"..Time.zone.now).group("date(created_at)") 
-   	@balance_win  = Trade.where(:User_id => current_user.id).where(created_at: "2015-07-15"..Time.zone.now).where("BrokerAccount_id in (?)", [2,3]).group("date(created_at)")
-   	@suma_win     = @balance_win.sum(:Payout)
-   	######### fin grafica 2 #####
-   	######### grafica 1
-    # @categorias2  = BrokerAccount.where("id in (?)", [2,3])
-    # @categorias2.each do |e|
-    # #@categorias2  = BrokerAccount.where(:id => 2)
-    # @join = AccountBalance.where(:broker_account_id => e.id).pluck("Amount")
-    # 	Rails.logger.info "Resultado #{@join}  "
-    # end
-       
-   @sum   = Trade.select("Amount","Payout","created_at").where(:User_id => current_user.id).where(created_at: "2015-07-15"..Time.zone.now).where(result: "WIN").where("BrokerAccount_id in (?)", [2,3]).group("date(created_at)")
-   Rails.logger.info "Suma #{@sum.sum(:Amount)} "
-
-   @suma  = @sum.group_by { |t| 
-   	t = (Float(t.Amount) + Float(t.Payout))
-   }
-
- 	######### fin grafica 3 #####
 
 
   end
