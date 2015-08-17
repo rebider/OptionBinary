efd5203ca339da0eb1aa28a7a436330e4c7e03b7
Juan Carlos Santiago <krlos013@gmail.com>
2015-08-17 13:59:49 -0500

    19-08-2015 14

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,8 +1,8 @@
 module DashboardHelper
 	def trades_chart_data(start = 3.weeks.ago)
-		won_by_day  = Trade.where(:User_id => current_user.id).where(:Result => 'WON').trades_grouped_by_date(start)
-		tie_by_day  = Trade.where(:User_id => current_user.id).where(:Result => 'TIE').trades_grouped_by_date(start)
-		lost_by_day = Trade.where(:User_id => current_user.id).where(:Result => 'LOST').trades_grouped_by_date(start)
+		won_by_day  = Trade.where(:User_id => current_user.id).where(:result => 'WON').trades_grouped_by_date(start)
+		tie_by_day  = Trade.where(:User_id => current_user.id).where(:result => 'TIE').trades_grouped_by_date(start)
+		lost_by_day = Trade.where(:User_id => current_user.id).where(:result => 'LOST').trades_grouped_by_date(start)
 
 		(start.to_date..Time.zone.today).map do |date|
 			{
@@ -19,9 +19,9 @@ module DashboardHelper
 	end
 
 	def today_trades_chart_data
-		won_by_day = Trade.where(:User_id => current_user.id).where(:Result => 'WON').trades_grouped_by_date(Time.zone.today)
-		tie_by_day = Trade.where(:User_id => current_user.id).where(:Result => 'TIE').trades_grouped_by_date(Time.zone.today)
-		lost_by_day = Trade.where(:User_id => current_user.id).where(:Result => 'LOST').trades_grouped_by_date(Time.zone.today)
+		won_by_day = Trade.where(:User_id => current_user.id).where(:result => 'WON').trades_grouped_by_date(Time.zone.today)
+		tie_by_day = Trade.where(:User_id => current_user.id).where(:result => 'TIE').trades_grouped_by_date(Time.zone.today)
+		lost_by_day = Trade.where(:User_id => current_user.id).where(:result => 'LOST').trades_grouped_by_date(Time.zone.today)
 
 
 
