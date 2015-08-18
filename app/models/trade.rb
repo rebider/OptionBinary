494ec281f1fcf2da8ffca6d5afe8bec69065e015
Miguel Bahena <miguel_bahena@hotmail.com>
2015-08-17 22:22:09 -0500

    ok

--- app/models/trade.rb
+++ app/models/trade.rb
@@ -22,14 +22,14 @@ class Trade < ActiveRecord::Base
 
   def self.trades_grouped_by_date(start)
   	trades = where(created_at: start.beginning_of_day..Time.zone.now)
-  	trades = trades.group("date(created_at)")
-  	trades = trades.select("date(created_at), count(\"#{:Result}\") as total_trades, SUM(\"#{:Payout}\"::float) as profit")
+    trades = trades.group("created_at")
+  	trades = trades.select("created_at, count(\"#{:Result}\") as total_trades, SUM(\"#{:Payout}\"::float) as profit")
   	trades.group_by { |o| o.created_at.to_date }
   end
 
   def self.profit_grouped_by_date(start)
   	trades = where(created_at: start.beginning_of_day..Time.zone.now)
-  	trades = trades.group("date(created_at)")
+  	trades = trades.group("created_at")
   	trades = trades.select("created_at, sum(payout::integer) as profit")
   	trades.group_by { |o| o.created_at.to_date }
   end
