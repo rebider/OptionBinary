431c9d7ca519a8fc376cd06b251a7046a9993797
Miguel Bahena <miguel_bahena@hotmail.com>
2015-09-06 22:37:51 -0500

    Signed-off-by: Miguel Bahena <miguel_bahena@hotmail.com>

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,7 +1,8 @@
 class Admin < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  attr_accessible :email, :password, :encrypted_password
-  devise :database_authenticatable, :trackable, :timeoutable, :lockable 
+  attr_accessible :email, :password, :encrypted_password, :remember_me
+  devise :database_authenticatable, :trackable, :timeoutable, :lockable, :rememberable
 
+  attr_accessor :login
 end
