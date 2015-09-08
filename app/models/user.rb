7cdf83d2681936d5637226ae5f5e6a8c730480cf
Miguel Bahena <miguel_bahena@hotmail.com>
2015-09-07 23:33:33 -0500

    Signed-off-by: Miguel Bahena <miguel_bahena@hotmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ActiveRecord::Base
       where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
         user.provider = auth.provider
         user.uid = auth.uid
-        #user.email = auth.info.email
+        user.email = auth.info.email
         #user.email= auth.info.email if email_is_verified
         user.name = auth.info.name
         user.avatar = auth.info.image
