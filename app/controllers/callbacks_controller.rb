835100139aefd6ed640e9bee015a765bc0c18fc8
Miguel Bahena <miguel_bahena@hotmail.com>
2015-09-07 23:58:54 -0500

    Signed-off-by: Miguel Bahena <miguel_bahena@hotmail.com>

--- app/controllers/callbacks_controller.rb
+++ app/controllers/callbacks_controller.rb
@@ -1,6 +1,11 @@
 class CallbacksController < Devise::OmniauthCallbacksController
     def facebook
-        @user = User.from_omniauth(request.env["omniauth.auth"])
-        sign_in_and_redirect @user
+    	if request.env["omniauth.auth"].info.email.blank?
+	      redirect_to "/users/auth/facebook?auth_type=rerequest&scope=email"
+	    else
+	     	@user = User.from_omniauth(request.env["omniauth.auth"])
+        	sign_in_and_redirect @user
+	    end
+        
     end
 end
